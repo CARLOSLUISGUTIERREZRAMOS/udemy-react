279c43a7be86687127096cf5449fc3c9916cdaee
CGUTIERREZ <carlos5t@hotmail.com>
2020-07-13 22:09:13 -0500

    Iniciando conocimiento con compenentes funcionales

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,20 +1,25 @@
-// import React, { Fragment } from 'react' //imr snippet
 import React from 'react' //imr snippet
+import PropTypes from 'prop-types'
+// import React, { Fragment } from 'react' //imr snippet
 
 // FC - Functional Component
 
-const PrimeraApp = ( props ) => {
-
-    const saludo = 'Hola mundo const';
-
-    console.log(props)
+const PrimeraApp = ({ saludo, subtitulo }) => {
 
     return (
         <>
             <h1>{ saludo }</h1>
-            <p>Mi primera aplicacion</p>
+            <p>{ subtitulo }</p>
         </>
     )
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo!'
 }
 
 export default PrimeraApp;
